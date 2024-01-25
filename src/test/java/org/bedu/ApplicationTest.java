50b778a56ec282cf1416380243ef6164b0119829
dylanestrada <dylaneb9861@gmail.com>
2024-01-24 20:41:55 -0600

    first commit

---
+++ src/test/java/org/bedu/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.bedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+	//Smoke Test
+	@Test
+	void contextLoads() {
+	}
+
+}
